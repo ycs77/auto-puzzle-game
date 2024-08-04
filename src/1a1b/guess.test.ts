8512000232650d61c2603d6318095b7513733356
Lucas Yang <yangchenshin77@gmail.com>
2024-08-04 22:00:22 +0800

    Improve tests

--- src/1a1b/guess.test.ts
+++ src/1a1b/guess.test.ts
@@ -2,81 +2,124 @@ import { getPossibleAnswers, guess1A1B } from './guess'
 import type { Item } from './guess'
 import { resetRng } from './number'
 
-test('can guess 1A1B to answer is 7493', () => {
+test('can guess 1A1B and remaining only one answer', () => {
   const seed = 42
-
   resetRng(seed)
 
   expect(guess1A1B({
     history: [],
     seed,
-  })).toStrictEqual({ guess: '0514', isAnswer: false })
+  })).toStrictEqual({ guess: '0123', isAnswer: false })
 
   expect(guess1A1B({
     history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
+      { guess: '0123', hint: { a: 1, b: 0 } },
     ],
     seed,
-  })).toStrictEqual({ guess: '6837', isAnswer: false })
+  })).toStrictEqual({ guess: '0456', isAnswer: false })
 
   expect(guess1A1B({
     history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
-      { guess: '6837', hint: { a: 0, b: 2 } },
+      { guess: '0123', hint: { a: 1, b: 0 } },
+      { guess: '0456', hint: { a: 1, b: 0 } },
     ],
     seed,
-  })).toStrictEqual({ guess: '3208', isAnswer: false })
+  })).toStrictEqual({ guess: '0798', isAnswer: false })
 
   expect(guess1A1B({
     history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
-      { guess: '6837', hint: { a: 0, b: 2 } },
-      { guess: '3208', hint: { a: 0, b: 1 } },
+      { guess: '0123', hint: { a: 1, b: 0 } },
+      { guess: '0456', hint: { a: 1, b: 0 } },
+      { guess: '0798', hint: { a: 1, b: 1 } },
     ],
     seed,
-  })).toStrictEqual({ guess: '8975', isAnswer: false })
+  })).toStrictEqual({ guess: '7158', isAnswer: false })
 
   expect(guess1A1B({
     history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
-      { guess: '6837', hint: { a: 0, b: 2 } },
-      { guess: '3208', hint: { a: 0, b: 1 } },
-      { guess: '8975', hint: { a: 0, b: 2 } },
+      { guess: '0123', hint: { a: 1, b: 0 } },
+      { guess: '0456', hint: { a: 1, b: 0 } },
+      { guess: '0798', hint: { a: 1, b: 1 } },
+      { guess: '7158', hint: { a: 1, b: 0 } },
     ],
     seed,
-  })).toStrictEqual({ guess: '1689', isAnswer: false })
+  })).toStrictEqual({ guess: '9428', isAnswer: false })
 
   expect(guess1A1B({
     history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
-      { guess: '6837', hint: { a: 0, b: 2 } },
-      { guess: '3208', hint: { a: 0, b: 1 } },
-      { guess: '8975', hint: { a: 0, b: 2 } },
-      { guess: '1689', hint: { a: 0, b: 1 } },
-    ],
-    seed,
-  })).toStrictEqual({ guess: '2756', isAnswer: false })
-
-  expect(guess1A1B({
-    history: [
-      { guess: '0514', hint: { a: 0, b: 1 } },
-      { guess: '6837', hint: { a: 0, b: 2 } },
-      { guess: '3208', hint: { a: 0, b: 1 } },
-      { guess: '8975', hint: { a: 0, b: 2 } },
-      { guess: '1689', hint: { a: 0, b: 1 } },
-      { guess: '2756', hint: { a: 0, b: 1 } },
+      { guess: '0123', hint: { a: 1, b: 0 } },
+      { guess: '0456', hint: { a: 1, b: 0 } },
+      { guess: '0798', hint: { a: 1, b: 1 } },
+      { guess: '7158', hint: { a: 1, b: 0 } },
+      { guess: '9428', hint: { a: 1, b: 1 } },
     ],
     seed,
   })).toStrictEqual({ guess: '7493', isAnswer: true })
 })
 
+test('can guess 1A1B remaining not one answer', () => {
+  const seed = 42
+  resetRng(seed)
+
+  expect(guess1A1B({
+    history: [],
+    seed,
+  })).toStrictEqual({ guess: '0123', isAnswer: false })
+
+  expect(guess1A1B({
+    history: [
+      { guess: '0123', hint: { a: 0, b: 3 } },
+    ],
+    seed,
+  })).toStrictEqual({ guess: '4012', isAnswer: false })
+
+  expect(guess1A1B({
+    history: [
+      { guess: '0123', hint: { a: 0, b: 3 } },
+      { guess: '4012', hint: { a: 0, b: 3 } },
+    ],
+    seed,
+  })).toStrictEqual({ guess: '1260', isAnswer: false })
+
+  expect(guess1A1B({
+    history: [
+      { guess: '0123', hint: { a: 0, b: 3 } },
+      { guess: '4012', hint: { a: 0, b: 3 } },
+      { guess: '1260', hint: { a: 2, b: 0 } },
+    ],
+    seed,
+  })).toStrictEqual({ guess: '3240', isAnswer: false })
+
+  expect(guess1A1B({
+    history: [
+      { guess: '0123', hint: { a: 0, b: 3 } },
+      { guess: '4012', hint: { a: 0, b: 3 } },
+      { guess: '1260', hint: { a: 2, b: 0 } },
+      { guess: '3240', hint: { a: 1, b: 2 } },
+    ],
+    seed,
+  })).toStrictEqual({ guess: '1234', isAnswer: false })
+
+  expect(guess1A1B({
+    history: [
+      { guess: '0123', hint: { a: 0, b: 3 } },
+      { guess: '4012', hint: { a: 0, b: 3 } },
+      { guess: '1260', hint: { a: 2, b: 0 } },
+      { guess: '3240', hint: { a: 1, b: 2 } },
+      { guess: '1234', hint: { a: 4, b: 0 } },
+    ],
+    seed,
+  })).toStrictEqual({ guess: '1234', isAnswer: true })
+})
+
 test('auto guess 1A1B', () => {
   const answer = '7493'
-  const seed = 42
   const history: (Item & { answerCount: number })[] = []
-  let i = 0
 
+  const seed = 42
   resetRng(seed)
+
+  let i = 0
 
   while (i < 10) {
     // 程式開始猜數字
@@ -103,12 +146,11 @@ test('auto guess 1A1B', () => {
   }
 
   expect(history).toStrictEqual([
-    { guess: '0514', hint: { a: 0, b: 1 }, answerCount: 5040 },
-    { guess: '6837', hint: { a: 0, b: 2 }, answerCount: 5040 },
-    { guess: '3208', hint: { a: 0, b: 1 }, answerCount: 504 },
-    { guess: '8975', hint: { a: 0, b: 2 }, answerCount: 114 },
-    { guess: '1689', hint: { a: 0, b: 1 }, answerCount: 25 },
-    { guess: '2756', hint: { a: 0, b: 1 }, answerCount: 6 },
+    { guess: '0123', hint: { a: 1, b: 0 }, answerCount: 5040 },
+    { guess: '0456', hint: { a: 1, b: 0 }, answerCount: 480 },
+    { guess: '0798', hint: { a: 1, b: 1 }, answerCount: 42 },
+    { guess: '7158', hint: { a: 1, b: 0 }, answerCount: 12 },
+    { guess: '9428', hint: { a: 1, b: 1 }, answerCount: 2 },
     { guess: '7493', hint: { a: 4, b: 0 }, answerCount: 1 },
   ])
 })
