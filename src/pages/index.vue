08801e4b553aab167c42a2e65de643be723031ec
Lucas Yang <yangchenshin77@gmail.com>
2024-09-07 15:18:04 +0800

    Update home list style

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,16 +2,16 @@
   <div class="py-8">
     <h1 class="text-2xl text-center font-bold">程式解題</h1>
 
-    <div class="mt-4 flex flex-wrap gap-2 justify-center">
-      <RouterLink to="/sudoku" class="inline-block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white rounded-md transition-colors">
+    <div class="mt-4 max-w-xs px-4 mx-auto flex flex-col gap-4 justify-center">
+      <RouterLink to="/sudoku" class="block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white text-center rounded-md transition-colors">
         數獨
       </RouterLink>
 
-      <RouterLink to="/1a1b-answer" class="inline-block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white rounded-md transition-colors">
+      <RouterLink to="/1a1b-answer" class="block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white text-center rounded-md transition-colors">
         1A1B 程式答題
       </RouterLink>
 
-      <RouterLink to="/1a1b-ask" class="inline-block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white rounded-md transition-colors">
+      <RouterLink to="/1a1b-ask" class="block px-4 py-1.5 bg-emerald-500 hover:bg-emerald-600 text-white text-center rounded-md transition-colors">
         1A1B 程式出題
       </RouterLink>
     </div>
